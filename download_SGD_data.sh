9649d43260cae6ee7bf849d2757d5cf9d087f994
Thibault-Poinsignon <thibault.poinsignon@protonmail.com>
2021-02-05 14:58:28 +0100

    fix #5 improve SGD data download

--- download_SGD_data.sh
+++ download_SGD_data.sh
@@ -1,20 +1,33 @@
 
 #####SGD_data#####
 
-mkdir SGD_data
-cd SGD_data
 
-wget http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/SGD_features.tab
-wget http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/chromosome_length.tab
-wget http://sgd-archive.yeastgenome.org/curation/literature/biochemical_pathways.tab
-wget http://sgd-archive.yeastgenome.org/curation/literature/gene_literature.tab
-wget http://sgd-archive.yeastgenome.org/curation/literature/interaction_data.tab
-wget http://sgd-archive.yeastgenome.org/curation/literature/phenotype_data.tab
-wget http://sgd-archive.yeastgenome.org/curation/literature/go_terms.tab
-wget http://sgd-archive.yeastgenome.org/curation/literature/go_slim_mapping.tab
-wget http://sgd-archive.yeastgenome.org/curation/literature/go_protein_complex_slim.tab
-wget http://sgd-archive.yeastgenome.org/curation/literature/gene_association.sgd.gaf.gz
-wget http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/dbxref.tab
-wget http://sgd-archive.yeastgenome.org/curation/calculated_protein_info/protein_properties.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/SGD_features.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/chromosome_length.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/literature/biochemical_pathways.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/literature/gene_literature.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/literature/interaction_data.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/literature/phenotype_data.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/literature/go_terms.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/literature/go_slim_mapping.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/literature/go_protein_complex_slim.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/literature/gene_association.sgd.gaf.gz
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/dbxref.tab
+wget -P SGD_data http://sgd-archive.yeastgenome.org/curation/calculated_protein_info/protein_properties.tab
 
-cd ..
+#####SGD_README#####
+
+
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/SGD_features.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/chromosome_length.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/literature/biochemical_pathways.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/literature/gene_literature.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/literature/interaction_data.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/literature/phenotype_data.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/literature/go_terms.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/literature/go_slim_mapping.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/literature/go_protein_complex_slim.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/literature/gene_association.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/dbxref.README
+wget -P SGD_data/SGD_readme http://sgd-archive.yeastgenome.org/curation/calculated_protein_info/protein_properties.README
+
