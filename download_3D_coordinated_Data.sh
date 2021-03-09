c7a79a9580079c7e1f8ca3230f92406c74a4313e
Thibault-Poinsignon <thibault.poinsignon@protonmail.com>
2021-03-09 14:49:58 +0100

    Fix #25 update 3D coordinates download

--- download_3D_coordinated_Data.sh
+++ download_3D_coordinated_Data.sh
@@ -1,5 +1,5 @@
-
-#####3D_coordinated_Data#####
-
+#!/usr/bin/env bash
 
 wget -P data/3D_coordinates https://noble.gs.washington.edu/proj/yeast-architecture/figures/3dfigures/3dmodel.pdb
+
+sed 's/ \{1,\}/,/g' data/3D_coordinates/3dmodel.pdb > data/3D_coordinates/3dmodel.csv
